4ff8a63d9fa95e448a4a8d6d8c738f6e24261a0f
picawawa4000 <114023995+picawawa4000@users.noreply.github.com>
2024-09-13 15:48:19 -0400

    come on Julian you got this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NotCopiedDrive.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Drive that someone with a soul actually wrote")
+public class NotCopiedDrive extends LinearOpMode {
+
+    //NO PASTING
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
