5e388616dad9a4d25c1ea02130fc82132bce68d7
Epp0k <julianschmitt.0401@gmail.com>
2024-10-16 16:19:18 -0400

    Coded for backup autonomous. probably wont work.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_parts.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+public class Autonomous_parts {
+
+     private LinearOpMode myOpMode = null;
+     private DcMotor leftFrontDrive = null;
+     private DcMotor leftBackDrive = null;
+     private DcMotor rightFrontDrive = null;
+     private DcMotor rightBackDrive = null;
+
+      public static final double MID_SERVO       =  0.5 ;
+      public static final double HAND_SPEED      =  0.02 ;
+      public static final double ARM_UP_POWER    =  0.45 ;
+      public static final double ARM_DOWN_POWER  = -0.45 ;
+
+     public static final double INCREMENT   = 0.05;
+     public static final int    CYCLE_MS    =   50;
+     public static final double MAX_POS     =  0.5;
+     public static final double MIN_POS     =  0.0;
+
+
+     public Autonomous_parts(LinearOpMode opmode) {
+         myOpMode = opmode;
+     }
+
+     public void init(){
+         leftFrontDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_front_drive");
+         leftBackDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_back_drive");
+         rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_front_drive");
+         rightBackDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_back_drive");
+
+         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+         myOpMode.telemetry.addData(">", "Hardware Initialized: Bot Ready!");
+         myOpMode.telemetry.update();
+     }
+
+     public void driveRobot(double axial, double lateral, double yaw) {
+         double leftFrontPower  = axial + lateral + yaw;
+         double rightFrontPower = axial - lateral - yaw;
+         double leftBackPower   = axial - lateral + yaw;
+         double rightBackPower  = axial + lateral - yaw;
+         double max;
+
+         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+         max = Math.max(max, Math.abs(leftBackPower));
+         max = Math.max(max, Math.abs(rightBackPower));
+
+         if (max > 1.0) {
+             leftFrontPower  /= max;
+             rightFrontPower /= max;
+             leftBackPower   /= max;
+             rightBackPower  /= max;
+         }
+
+
+         setDrivePower(leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
+     }
+
+     public void setDrivePower(double leftFrontWheel, double rightFrontWheel, double leftBackWheel, double rightBackWheel) {
+         leftFrontDrive.setPower(leftFrontWheel);
+         rightFrontDrive.setPower(rightFrontWheel);
+         leftBackDrive.setPower(leftBackWheel);
+         rightBackDrive.setPower(rightBackWheel);
+     }
+ }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backup_Autonomous.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous(name="Backup_nonecammera", group="Test Robot")
+
+public class backup_Autonomous extends LinearOpMode {
+
+     Autonomous_parts  powerClaw = new Autonomous_parts(this);
+
+     private ElapsedTime     runtime = new ElapsedTime();
+
+
+     static final double     FORWARD_SPEED = 0.5;
+     static final double     TURN_SPEED    = 0.5;
+     static final double     SIDE_SPEED    = 0.5;
+
+     @Override
+     public void runOpMode() {
+
+         powerClaw.init();
+
+         telemetry.addData("Status", "Ready to run");
+         telemetry.update();
+
+         waitForStart();
+
+         powerClaw.driveRobot(FORWARD_SPEED, 0, 0);
+         runtime.reset();
+         while (opModeIsActive() && (runtime.seconds() < 2.3)) {
+             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+             telemetry.update();
+         }
+
+
+         powerClaw.driveRobot(0, 0, TURN_SPEED);
+         powerClaw.driveRobot(0, 0, -TURN_SPEED);
+         runtime.reset();
+         while (opModeIsActive() && (runtime.seconds() < 0.75)) {
+             telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+             telemetry.update();
+         }
+
+
+         powerClaw.driveRobot(FORWARD_SPEED, 0, 0);
+         runtime.reset();
+         while (opModeIsActive() && (runtime.seconds() < 2.5)) {
+             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+             telemetry.update();
+         }
+
+
+         powerClaw.driveRobot(0, SIDE_SPEED, 0);
+         runtime.reset();
+         while (opModeIsActive() && (runtime.seconds() < 2.5)) {
+             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+             telemetry.update();
+         }
+
+          runtime.reset();
+          while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+              telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
+              telemetry.update();
+          }
+
+         powerClaw.driveRobot(0, 0, 0);
+
+         telemetry.addData("Path", "Complete");
+         telemetry.update();
+         sleep(1000);
+     }
+ }
