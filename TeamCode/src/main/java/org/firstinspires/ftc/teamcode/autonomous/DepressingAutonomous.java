f7ef9d740f796588782b8f4dcd22dd1e3181822c
picawawa4000 <114023995+picawawa4000@users.noreply.github.com>
2024-12-09 15:34:19 -0500

    why does Mr. H require so much of me?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DepressingAutonomous.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "DepressingAutonomous")
+public class DepressingAutonomous extends LinearOpMode {
+    /**
+     * Runs the OpMode if the camera failed to pick up the robot's location.
+     * **/
+    public static void runFallbackOpMode() {
+        LinearOpMode thiz = AutoUtil.getOpMode();
+
+        while (thiz.opModeIsActive()) {
+            PinpointTest.odometry.update();
+
+            AutoUtil.ChainTelemetry.assertAndGet()
+                    .add("Data in inches and degrees.")
+                    .add("Odometer X position", PinpointTest.odometry.getPosition().getX(DistanceUnit.INCH))
+                    .add("Odometer Z position", PinpointTest.odometry.getPosition().getY(DistanceUnit.INCH))
+                    .add("Odometer angle", PinpointTest.odometry.getPosition().getHeading(AngleUnit.DEGREES))
+                    .update();
+        }
+    }
+
+    @Override
+    public void runOpMode() {
+        AutoUtil.setOpMode(this);
+        AutoUtil.ChainTelemetry.init(this.telemetry);
+
+        PinpointTest.internalRunOpMode(true);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PinpointTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PinpointTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraException;
@@ -10,58 +11,84 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamServer;
 import org.firstinspires.ftc.teamcode.external.GoBildaPinpointDriver;
 
-import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Autonomous(name="PinpointTest")
 public class PinpointTest extends LinearOpMode {
-    public GoBildaPinpointDriver odometry;
+    public static GoBildaPinpointDriver odometry;
     public static AtomicReference<CameraHandler.FieldPos> lastPosition = new AtomicReference<>(null);
 
-    public void runOpMode() {
-        AutoUtil.setOpMode(this);
-        AutoUtil.ChainTelemetry.init(this.telemetry);
+    private static void runProperOpMode() {
+        LinearOpMode thiz = AutoUtil.getOpMode();
 
-        this.odometry = this.hardwareMap.get(GoBildaPinpointDriver.class, GlobalConstants.ODOMETRY_NAME);
-        this.odometry.setOffsets(GlobalConstants.ODOMETRY_X_OFFSET, GlobalConstants.ODOMETRY_Y_OFFSET);
-        this.odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        this.odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        this.odometry.resetPosAndIMU();
+        while (thiz.opModeIsActive()) {
+            PinpointTest.odometry.update();
+
+            AutoUtil.ChainTelemetry.assertAndGet()
+                    .add("Data in inches and degrees.")
+                    .add("Odometer X position", PinpointTest.odometry.getPosition().getX(DistanceUnit.INCH))
+                    .add("Odometer Z position", PinpointTest.odometry.getPosition().getY(DistanceUnit.INCH))
+                    .add("Odometer angle", PinpointTest.odometry.getPosition().getHeading(AngleUnit.DEGREES))
+                    .update();
+        }
+    }
+
+    /**
+     * This is an ugly solution, but the poor programmers always have too much demanded of them...
+     * **/
+    public static void internalRunOpMode(boolean allowFallback) {
+        LinearOpMode thiz = AutoUtil.getOpMode();
+
+        PinpointTest.odometry = thiz.hardwareMap.get(GoBildaPinpointDriver.class, GlobalConstants.ODOMETRY_NAME);
+        PinpointTest.odometry.setOffsets(GlobalConstants.ODOMETRY_X_OFFSET, GlobalConstants.ODOMETRY_Y_OFFSET);
+        PinpointTest.odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        PinpointTest.odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        PinpointTest.odometry.resetPosAndIMU();
 
         CameraHandler.CameraFrameCallback callback;
         Camera camera;
 
         try {
             callback = new CameraHandler.CameraFrameCallback((frame) -> PinpointTest.lastPosition.set(CameraHandler.getLocationOnBoard(frame)));
-            camera = CameraHandler.createCamera(this.hardwareMap, GlobalConstants.CAMERA_X_SIZE, GlobalConstants.CAMERA_Y_SIZE, callback);
+            camera = CameraHandler.createCamera(thiz.hardwareMap, GlobalConstants.CAMERA_X_SIZE, GlobalConstants.CAMERA_Y_SIZE, callback);
             if (camera == null) throw new RuntimeException("Camera not created (check logs for details)");
             CameraStreamServer.getInstance().setSource(callback.getCameraStreamSource());
         } catch (CameraException | AutoUtil.OpModeInterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        while (PinpointTest.lastPosition.get() == null) if (!this.opModeInInit()) {
+        while (PinpointTest.lastPosition.get() == null) if (!thiz.opModeInInit()) {
             camera.close();
-            throw new RuntimeException(new AutoUtil.OpModeInterruptedException("Interrupted while waiting for camera frame pos!"));
+            RobotLog.e("Interrupted while waiting for camera frame pos!");
+            break;
         }
 
-        CameraHandler.FieldPos lastPositionCurrentValue = PinpointTest.lastPosition.get();
-        this.odometry.setPosition(new GoBildaPinpointDriver.Pose2D(DistanceUnit.INCH, lastPositionCurrentValue.x, lastPositionCurrentValue.y, AngleUnit.RADIANS, lastPositionCurrentValue.theta));
+        boolean isFallback;
+
+        if (PinpointTest.lastPosition.get() != null) {
+            CameraHandler.FieldPos lastPositionCurrentValue = PinpointTest.lastPosition.get();
+            PinpointTest.odometry.setPosition(new GoBildaPinpointDriver.Pose2D(DistanceUnit.INCH, lastPositionCurrentValue.x, lastPositionCurrentValue.y, AngleUnit.RADIANS, lastPositionCurrentValue.theta));
+            isFallback = false;
+        } else {
+            if (!allowFallback) throw new RuntimeException(new AutoUtil.OpModeInterruptedException("OpMode interrupted in init!"));
+            PinpointTest.odometry.setPosition(new GoBildaPinpointDriver.Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0));
+            isFallback = true;
+        }
 
         callback.terminate();
         camera.close();
 
-        this.waitForStart();
+        thiz.waitForStart();
 
-        while (this.opModeIsActive()) {
-            this.odometry.update();
+        if (isFallback) DepressingAutonomous.runFallbackOpMode();
+        else PinpointTest.runProperOpMode();
+    }
 
-            AutoUtil.ChainTelemetry.assertAndGet()
-                    .add("Data in inches and degrees.")
-                    .add("Odometer X position", this.odometry.getPosition().getX(DistanceUnit.INCH))
-                    .add("Odometer Z position", this.odometry.getPosition().getY(DistanceUnit.INCH))
-                    .add("Odometer angle", this.odometry.getPosition().getHeading(AngleUnit.DEGREES))
-                    .update();
-        }
+    @Override
+    public void runOpMode() {
+        AutoUtil.setOpMode(this);
+        AutoUtil.ChainTelemetry.init(this.telemetry);
+
+        PinpointTest.internalRunOpMode(false);
     }
 }
